679fa486371f58e781d81d14fc696e865c0475e8
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-11-21 13:19:35 +0700

    Selesai TodoApp

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -22,7 +22,8 @@ module.exports = (app) => {
         res.json(data);
     })
     app.delete('/todo/:item', (req, res) => {
-        data = data.filter((todo) => todo.item.replace(/ /g, '-') !== req.params.item)
+        data = data.filter((todo) => todo.item.replace(/ /g, '-') !== req.params.item);
+console.log(data)
         res.json(data);
     })
 }
