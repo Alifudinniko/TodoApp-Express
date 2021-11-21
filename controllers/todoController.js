f1ac02d755ae76903dcd96eb10e9073bb181fe54
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-11-21 12:59:22 +0700

    add new

---
+++ controllers/todoController.js
@@ -0,0 +1,28 @@
+let data = [
+    {
+        item: 'node js'
+    }, {
+        item: 'express js'
+    }
+
+];
+
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+module.exports = (app) => {
+
+    app.get('/todo', (req, res) => {
+        res.render('todo', { todos: data })
+    })
+    app.post('/todo', urlencodedParser, (req, res) => {
+        data.push(req.body);
+        console.log(data)
+        res.json(data);
+    })
+    app.delete('/todo/:item', (req, res) => {
+        data = data.filter((todo) => todo.item.replace(/ /g, '-') !== req.params.item)
+        res.json(data);
+    })
+}
